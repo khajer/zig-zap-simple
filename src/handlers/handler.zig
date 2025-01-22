98c7327fb185a71823bb8f3de59238d9d192094f
itsara <khajer@gmail.com>
2025-01-23 03:17:55 +0700

    change response from string html to json

--- src/handlers/handler.zig
+++ src/handlers/handler.zig
@@ -1,6 +1,8 @@
 const zap = @import("zap");
 const std = @import("std");
 
+const Resp = struct { status_code: u32, status_message: []const u8 };
+
 pub fn on_request(r: zap.Request) void {
     if (r.path) |the_path| {
         std.debug.print("PATH: {s}\n", .{the_path});
@@ -9,5 +11,15 @@ pub fn on_request(r: zap.Request) void {
     if (r.query) |the_query| {
         std.debug.print("QUERY: {s}\n", .{the_query});
     }
-    r.sendBody("<html><body><h1>Hello from ZAP!!!</h1></body></html>") catch return;
+
+    var json_resp: []const u8 = undefined;
+    const resp = Resp{ .status_code = 200, .status_message = "ok" };
+    var buf: [100]u8 = undefined;
+    if (zap.stringifyBuf(&buf, resp, .{})) |json| {
+        json_resp = json;
+    } else {
+        json_resp = "null";
+    }
+
+    r.sendJson(json_resp) catch return;
 }
