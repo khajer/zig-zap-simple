00eb75f6b3516aad2d2ba5c2c5fa4b5c9d02d7b9
itsara <khajer@gmail.com>
2025-02-02 01:13:08 +0700

    test return string ([]const u8)

--- src/handlers/handler.zig
+++ src/handlers/handler.zig
@@ -1,7 +1,20 @@
 const zap = @import("zap");
 const std = @import("std");
 
-const Resp = struct { status_code: u32, status_message: []const u8 };
+const Resp = struct { status_code: u8, status_message: []const u8 };
+
+fn generate_response(allocator: std.mem.Allocator) []const u8 {
+    const resp = Resp{ .status_code = 200, .status_message = "ok" };
+    var buf: [100]u8 = undefined;
+    if (zap.stringifyBuf(&buf, resp, .{})) |json| {
+        if (allocator.dupe(u8, json)) |json_resp| {
+            return json_resp;
+        } else |_| {
+            return "Error";
+        }
+    }
+    return "Error";
+}
 
 pub fn on_request(r: zap.Request) void {
     if (r.path) |the_path| {
@@ -11,15 +24,12 @@ pub fn on_request(r: zap.Request) void {
     if (r.query) |the_query| {
         std.debug.print("QUERY: {s}\n", .{the_query});
     }
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    const json_res = generate_response(allocator);
+    defer allocator.free(json_res);
 
-    var json_resp: []const u8 = undefined;
-    const resp = Resp{ .status_code = 200, .status_message = "ok" };
-    var buf: [100]u8 = undefined;
-    if (zap.stringifyBuf(&buf, resp, .{})) |json| {
-        json_resp = json;
-    } else {
-        json_resp = "null";
-    }
-
-    r.sendJson(json_resp) catch return;
+    r.sendJson(json_res) catch return;
 }
+
+test "test resonse" {}
